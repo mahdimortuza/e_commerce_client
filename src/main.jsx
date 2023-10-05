cdd66c485d4483a58cc69123ccb1000b4d5b26cb
mahdimortuza <mahdi.webx@gmail.com>
2023-10-05 09:53:58 +0600

    redux implemented

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./routs/Routes.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/Store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="bg-[#f8f8f8]">
-    <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </div>
   </React.StrictMode>
 );
