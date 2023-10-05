4dcd8eb51b997e01b9240cd2e407d6a42e736f88
mahdimortuza <mahdi.webx@gmail.com>
2023-10-05 09:43:17 +0600

    tab updated

--- src/pages/Home/AllProducts/Products/Products.jsx
+++ src/pages/Home/AllProducts/Products/Products.jsx
@@ -27,6 +27,143 @@ const Products = () => {
 
   return (
     <div className="mt-5">
+      <div className="block md:hidden">
+      <Tab.Group vertical>
+        <Tab.List className="flex flex-col rounded-xl bg-[#dfdfdfc5] p-1">
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Mouse
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Keyboard
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Joystick
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Monitor
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Chair
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Headphones
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Games
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Accessories
+          </Tab>
+          <Tab
+            className={({ selected }) =>
+              classNames(
+                "w-full rounded-lg py-1 text-sm font-medium text-black",
+                "ring-[#e6005c] ring-opacity-60 ring-offset-2 ring-offset-[#e6005c] focus:outline-none focus:ring-1",
+                selected ? "bg-[#e6005c] text-white shadow" : "text-black"
+              )
+            }
+          >
+            Combo
+          </Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <ProductsTab items={mouse}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={keyboard}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={joystick}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={monitor}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={chair}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={headphone}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={games}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={accessories}></ProductsTab>
+          </Tab.Panel>
+          <Tab.Panel>
+            <ProductsTab items={combo}></ProductsTab>
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
+      </div>
+
+      {/* tab for desktop */}
+      <div className="hidden md:block">
       <Tab.Group>
         <Tab.List className="flex rounded-xl bg-[#dfdfdfc5] p-1">
           <Tab
@@ -159,6 +296,7 @@ const Products = () => {
           </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
+      </div>
     </div>
   );
 };
