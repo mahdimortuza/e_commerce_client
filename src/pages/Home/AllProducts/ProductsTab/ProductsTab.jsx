0a5924756ded8757a9a97b3ef4a644bcc1ddc4b9
mahdimortuza <mahdi.webx@gmail.com>
2023-10-05 10:44:23 +0600

    loading data using rtk query

--- src/pages/Home/AllProducts/ProductsTab/ProductsTab.jsx
+++ src/pages/Home/AllProducts/ProductsTab/ProductsTab.jsx
@@ -4,7 +4,7 @@ const ProductsTab = ({ items }) => {
   return (
     <div className="mt-8">
       <div className="grid xs:grid-cols-2 md:grid-cols-5 gap-3 md:gap-3">
-        {items.map((product) => (
+        {items?.map((product) => (
           <ProductCard key={product._id} product={product}></ProductCard>
         ))}
       </div>
