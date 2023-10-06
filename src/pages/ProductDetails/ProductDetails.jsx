8a04f5d9ba9708163a7f01c7d4e7dada13ad4435
mahdimortuza <mahdi.webx@gmail.com>
2023-10-06 15:14:00 +0600

    product detail page added

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,10 +1,7 @@
-// import { useParams } from "react-router-dom";
-// import { useGetSingleProductQuery } from "../../redux/Store/Features/products/productsApi";
+
 
 const ProductDetails = () => {
-  // const { img, name, stock, price, description, category, tags } = product;
-  // const { data: products, isError, isLoading } = useGetSingleProductQuery(_id);
-  // console.log(products);
+  
 
   return (
     <div className="max-w-[1450px] px-5 md:px-0 mx-auto">
