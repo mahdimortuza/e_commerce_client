e747ee0cdcb8beb0bfdbe3a53fc7b2f612cd97e4
mahdimortuza <mahdi.webx@gmail.com>
2023-10-06 15:29:37 +0600

    single product error

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,32 @@
+import { useGetSingleProductQuery } from "../../redux/Store/Features/products/productsApi";
 
+const ProductDetails = ({ _id }) => {
+  const { data: products, isLoading, isError } = useGetSingleProductQuery(_id);
 
-const ProductDetails = () => {
-  
+  if (isLoading) {
+    return (
+      <div className="flex justify-center">
+        <progress className="progress w-56 mx-auto mt-10"></progress>
+      </div>
+    );
+  }
+
+  if (!isLoading && isError) {
+    return (
+      <div className="flex justify-center">
+        <h1 className="text-red-600 font-semibold text-md mt-10">
+          Please check your connection :)
+        </h1>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-[1450px] px-5 md:px-0 mx-auto">
       Product details page
+      {products.map((item) => (
+        <div key={item._id}>{item.name}</div>
+      ))}
     </div>
   );
 };
