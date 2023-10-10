d001b4fb23509fe88f34281592e1dcf12ccfb21c
mahdimortuza <mahdi.webx@gmail.com>
2023-10-10 11:23:16 +0600

    helmet on cart page added

--- src/pages/MyCart/MyCart.jsx
+++ src/pages/MyCart/MyCart.jsx
@@ -1,7 +1,12 @@
+import { Helmet } from "react-helmet";
 
 const MyCart = () => {
     return (
-        <div>
+        <div className="max-w-[1450px] px-5 md:px-0 mx-auto">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Cart || Console </title>
+        </Helmet>
             Cart Page
         </div>
     );
