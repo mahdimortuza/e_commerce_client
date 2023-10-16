398dc88cc8caef633b2b3c73a8767174975c2aaf
mahdimortuza <mahdi.webx@gmail.com>
2023-10-16 14:53:02 +0600

    api updated

--- src/redux/Store/Features/api/baseApi.js
+++ src/redux/Store/Features/api/baseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://e-commerce-server-rho.vercel.app" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
   endpoints: () => ({}),
 });
 export default baseApi;
